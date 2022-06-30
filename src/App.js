7a22acfa7a96877acbd97a632bb205797c25ceae
benjamin31200 <benjamindeneux@hotmail.com>
2022-06-30 17:41:00 +0200

    test avec componentDidUpdate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Mount from "./components/diMount.js";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <Mount />
+    </div>
+  );
+}
